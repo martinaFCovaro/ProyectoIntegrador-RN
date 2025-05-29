f9b8c9d5bbdf220a7933cb92e4429d314b7b94d0
santosbarral <sbarral@udesa.edu.ar>
2025-05-29 18:50:09 -0300

    screen perfil llegada de info

--- PI-ReactNative/src/screens/Perfil.js
+++ PI-ReactNative/src/screens/Perfil.js
@@ -1,12 +1,32 @@
+import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native';
 import { Component } from "react";
-import { auth } from '../firebase/config';
+import { db, auth } from "../firebase/config";
 
-class Perfil extends Component {
+export default class Perfil extends Component {
     constructor(props) {
         super(props)
         this.state = {
-
+            InfoUsuario: []
         }
+    }
+    componentDidMount() {
+        db.collection("users")
+            .where("owner", "==", auth.currentUser.email)
+            .onSnapshot(docs => {
+                let arrDocs = []
+                docs.forEach(
+                    (doc) => {
+                        arrDocs.push({
+                            id: doc.id,
+                            data: doc.data()
+                        })
+                    }
+                )
+
+                this.setState({
+                    InfoUsuario: arrDocs
+                }, () => console.log("Este es el estado", this.state))
+            })
     }
     logout() {
         auth.signOut()
@@ -15,15 +35,18 @@ class Perfil extends Component {
     }
     render() {
         return (
-
-<></>
-
-       
+            <View>
+                <Text>PERFI3LL</Text>
+                <Text>
+                    {
+                    this.state.InfoUsuario.length > 0
+                    ?
+                    <Text>{this.state.InfoUsuario[0].data.userName}</Text>
+                    :
+                    "vacio"
+                    }</Text>
+            </View>
         )
     }
 
-
-
-
 }
-export default Perfil;
