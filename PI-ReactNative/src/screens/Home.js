4da07c42c28f27492db18638d8c25c3b5bbc3f45
agustinajorba <ajorba@udesa.edu.ar>
2025-05-30 16:45:38 -0300

    Cambié el Home a un componennte de clase

--- PI-ReactNative/src/screens/Home.js
+++ PI-ReactNative/src/screens/Home.js
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { Component } from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, TextInput, ActivityIndicator } from 'react-native';
+import { db, auth } from '../firebase/config';
 
-export default function Home() {
-  return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
+
+export default class Home extends Component() {
+  constructor(props){
+    super(props)
+    this.state = {
+      posts: []
+    }
+  }
 }
